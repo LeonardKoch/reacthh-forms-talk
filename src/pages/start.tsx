89deded9df21415e18dcf5ff57bb934a37da0a6f
Leonard Koch <leonardkochlew@gmail.com>
2024-05-05 15:06:17 +0200

    Add basic form version

--- src/pages/start.tsx
+++ src/pages/start.tsx
@@ -1,10 +1,44 @@
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from '@/components/ui/label.tsx';
+import { Button } from '@/components/ui/button.tsx';
+import { Separator } from '@/components/ui/separator.tsx';
+
+
 export function Start() {
     return (
-        <div className="h-screen w-screen">
-            <div>
-                <h1 className="text-3xl font-bold">Welcome to Vite + React + Tailwind CSS</h1>
-                <p className="text-lg mt-2">Get started by editing <code>src/App.tsx</code></p>
-            </div>
+        <div>
+            <form className="p-4 flex flex-col gap-2" method="post" action={"/submit"}>
+                <Label htmlFor="country">Country</Label>
+                <Select name="company-type" required>
+                    <SelectTrigger className="w-[350px]">
+                        <SelectValue placeholder="Select Country" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="DE">Germany</SelectItem>
+                        <SelectItem value="US">United States of America</SelectItem>
+                    </SelectContent>
+                </Select>
+                <Separator className="my-4" />
+                <Label htmlFor="company">Company Name</Label>
+                <Input name="company" required minLength={3} />
+                <Label htmlFor="company">Company Type</Label>
+                <Select name="company-type" required>
+                    <SelectTrigger className="w-[350px]">
+                        <SelectValue placeholder="Select Company Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="GmbH">GmbH</SelectItem>
+                        <SelectItem value="UG">UG</SelectItem>
+                        <SelectItem value="AG">UG</SelectItem>
+                        <SelectItem value="LLC">LLC</SelectItem>
+                        <SelectItem value="C-Corp">C-Corp</SelectItem>
+                        <SelectItem value="S-Corp">S-Corp</SelectItem>
+                    </SelectContent>
+                </Select>
+                <Separator className="my-4" />
+                <Button type="submit">Submit</Button>
+            </form>
         </div>
     )
 }
