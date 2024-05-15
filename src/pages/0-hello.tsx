d36be74736c3ca340f85ce3cfbd0eafdcfe8078b
Leonard Koch <leonardkochlew@gmail.com>
2024-05-16 00:36:52 +0200

    Change title

--- src/pages/0-hello.tsx
+++ src/pages/0-hello.tsx
@@ -5,7 +5,7 @@ export function Hello() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
             <h1 className="text-3xl font-semibold">Hello! 👋</h1>
-            <h2 className="text-xl font-semibold">React Forms Talk</h2>
+            <h2 className="text-xl font-semibold">React Forms - From the Basics to the Tricky Bits</h2>
             <p>Leonard Koch</p>
         </div>
     )
