eef97e185d68957d2f2caaafbe8e56515e4b373b
Leonard Koch <leonardkochlew@gmail.com>
2024-05-16 00:31:19 +0200

    Remove line numbers from code examples

--- src/components/CodeDisplay.tsx
+++ src/components/CodeDisplay.tsx
@@ -2,22 +2,20 @@ import { Highlight, themes } from "prism-react-renderer"
 
 export const CodeDisplay = (props: { code?: string}) => (
     <Highlight
-        // theme={themes.github}
         theme={themes.vsDark}
         code={props.code || ''}
         language="tsx"
     >
         {({ style, tokens, getLineProps, getTokenProps }) => (
             <pre className="rounded p-4 text-wrap" style={style}>
-        {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line })}>
-                <span className="pr-4 w-8 inline-block">{i + 1}</span>
-                {line.map((token, key) => (
-                    <span key={key} {...getTokenProps({ token })} />
+                {tokens.map((line, i) => (
+                    <div key={i} {...getLineProps({ line })}>
+                        {line.map((token, key) => (
+                            <span key={key} {...getTokenProps({ token })} />
+                        ))}
+                    </div>
                 ))}
-            </div>
-        ))}
-      </pre>
+            </pre>
         )}
     </Highlight>
 )
