5f9c569e385fb7184e023ffcf84240a1cb2e8c66
Leonard Koch <leonardkochlew@gmail.com>
2024-05-16 00:40:53 +0200

    Make navigation collapsible

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ export function App() {
   return (
     <div className="h-screen w-screen">
         <ResizablePanelGroup  direction="horizontal">
-            <ResizablePanel minSize={10} defaultSize={15} maxSize={20} className="bg-neutral">
+            <ResizablePanel defaultSize={15} maxSize={20} className="bg-neutral">
                 <ul className="overflow-y-auto max-h-screen">
                     <NavItem to="/html-form">
                         1. Html Form
